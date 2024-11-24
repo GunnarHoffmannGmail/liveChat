8095dc69348f0d6615a4c8c9a892da9e5bbbea88
GunnarHoffmannGmail <122797236+GunnarHoffmannGmail@users.noreply.github.com>
2024-11-24 09:55:40 +0100

    gzugu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_mic_recorder import mic_recorder, speech_to_text
 
-def callback(recognized_text):
+def callback(recognized_text, *args, **kwargs):
     st.session_state.text_received.append(recognized_text)
 
 state = st.session_state
@@ -13,7 +13,7 @@ c1, c2 = st.columns(2)
 with c1:
     st.write("Convert speech to text:")
 with c2:
-    text = speech_to_text(language='en', use_container_width=True, just_once=False, key='STT', callback=callback)
+    text = speech_to_text(language='en', use_container_width=True, just_once=False, key='STT', callback=lambda recognized_text: callback(recognized_text))
 
 for text in state.text_received:
     st.text(text)
