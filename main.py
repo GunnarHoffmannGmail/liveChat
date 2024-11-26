3f17047d7b52508cfabd196fe43d9f2dddda38ef
GunnarHoffmannGmail <122797236+GunnarHoffmannGmail@users.noreply.github.com>
2024-11-26 13:51:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import streamlit.components.v1 as components
 st.title("Live Speech-to-Text App with Microphone")
 
 # Extract Google API key from secrets
-api_key = st.secrets["mykey"]
+api_key = st.secrets["mykey"]["private_key_id"]
 
 # Embed the HTML code using Streamlit's components.html function
 components.html(
     f"""
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang='en'>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>Live Speech-to-Text with Google API</title>
     </head>
     <body>
         <h1>Live Speech-to-Text App with Microphone</h1>
         <p>Press 'Start' to begin capturing your speech!</p>
         
-        <button id="startButton">Start Listening</button>
-        <button id="stopButton" disabled>Stop Listening</button>
-        <textarea id="transcript" rows="10" cols="100"></textarea>
+        <button id='startButton'>Start Listening</button>
+        <button id='stopButton' disabled>Stop Listening</button>
+        <textarea id='transcript' rows='10' cols='100'></textarea>
 
         <script>
             let mediaRecorder;
